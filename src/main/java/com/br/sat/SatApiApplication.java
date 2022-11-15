2f11e64e0c810c481ca01294f2e0d2c65269d859
alexrochadealbuquer <arda@NEUROTECH.INTRANET>
2022-11-15 16:06:24 -0300

    Projeto Criado

---
+++ src/main/java/com/br/sat/SatApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.sat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatApiApplication.class, args);
+	}
+
+}
